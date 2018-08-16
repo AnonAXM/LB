5863613ad8d9f32fa8c9bfb9c55e711c6bb7ac0d
AnonAXM <41913603+AnonAXM@users.noreply.github.com>
2018-08-16 18:42:43 +0800

    Update LB.sh

--- LB.sh
+++ LB.sh
@@ -21,21 +21,31 @@ echo "\033[34;1m
 \033[34;1m#####################################################
 
 \033[32;1m
-1.XATTACKER     2.TORSHAMMER
+1.Fake Call     2.Sms Jdid
 
-3.SCANCHAN      4.S3R3N7Y
+3.XATTACKER     4.TORSHAMMER
 
-5.METASPLOIT    6.MKBRUTUS
+5.SCANCHAN      6.S3R3N7Y
 
-7.VCRT          8.MBF
+7.METASPLOIT    8.MKBRUTUS
 
-9.BONUS
+9.VCRT          10.MBF
 
-Masukkan Nomor pilihan 👇 "
+99.BONUS        \033[31;1m00.Exit"
 
 read LB
-
 if [ $LB = 1 ] || [ $LB = 1 ]
+then 
+php run.php
+fi
+
+
+if [ $LB = 2 ] || [ $LB = 2 ]
+then 
+php jdid.php
+fi
+
+if [ $LB = 3 ] || [ $LB = 3 ]
 then 
 cd $HOME
 apt-get update
@@ -54,7 +64,7 @@ cpan App::yaml
 perl XAttacker.pl
 fi
 
-if [ $LB = 2 ] || [ $LB = 2 ]
+if [ $LB = 4 ] || [ $LB = 4 ]
 then
 cd $HOME
 apt-get update
@@ -63,13 +73,11 @@ apt-get install tor
 apt-get install python2
 pkg install tor
 git clone https://github.com/dotfighter/torshammer.git
-sleep 1
 echo "\033[31;1m SUCCES..."
-sleep 1
 cd LB && sh LB.sh
 fi
 
-if [ $LB = 3 ] || [ $LB = 3 ]
+if [ $LB = 5 ] || [ $LB = 5 ]
 then
 cd $HOME
 apt-get update && apt-get upgrade
@@ -84,40 +92,16 @@ apt-get install dos2unix
 dos2unix scanchan.sh
 sleep 1
 echo "\033[31;1m SUCCES..."
-sleep 1
 cd LB && sh LB.sh
 fi
 
-
-if [ $LB = 4 ] || [ $LB = 4 ]
-then
-cd $HOME
-pkg install git -y
-git clone https://github.com/T4P4N/53R3N17Y.git
-sleep 1
-echo "\033[31;1m SUCCES..."
-sleep 1
-cd LB && sh LB.sh
-fi
-
-
-if [ $LB = 5 ] || [ $LB = 5 ]
-then
-cd $HOME
-pkg update && pkg upgrade && pkg install curl && curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh 
-sleep 1
-echo "\033[31;1m SUCCES..."
-sleep 1
-cd LB && sh LB.sh
-fi
 
 if [ $LB = 6 ] || [ $LB = 6 ]
 then
 cd $HOME
-git clone https://github.com/mkbrutusproject/MKBRUTUS.git
-sleep 1
+pkg install git -y
+git clone https://github.com/T4P4N/53R3N17Y.git
 echo "\033[31;1m SUCCES..."
-sleep 1
 cd LB && sh LB.sh
 fi
 
@@ -125,15 +109,34 @@ fi
 if [ $LB = 7 ] || [ $LB = 7 ]
 then
 cd $HOME
-git clone https://github.com/LOoLzeC/Evil-create-framework.git
+pkg update && pkg upgrade && pkg install curl && curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh 
 sleep 1
 echo "\033[31;1m SUCCES..."
+cd LB && sh LB.sh
+fi
+
+if [ $LB = 8 ] || [ $LB = 8 ]
+then
+cd $HOME
+git clone https://github.com/mkbrutusproject/MKBRUTUS.git
 sleep 1
+echo "\033[31;1m SUCCES..."
 cd LB && sh LB.sh
 fi
 
 
-if [ $LB = 8 ] || [ $LB = 8 ]
+if [ $LB = 9 ] || [ $LB = 9 ]
+then
+cd $HOME
+git clone https://github.com/LOoLzeC/Evil-create-framework.git
+cd Evil-create-framework
+sleep 1
+echo "\033[31;1m SUCCES..."
+cd LB && sh LB.sh
+fi
+
+
+if [ $LB = 10 ] || [ $LB = 10 ]
 then
 cd $HOME
 pkg update && upgrade -y
@@ -142,22 +145,26 @@ pkg install git -y
 Install mechanize
 pip2 install mechanize
 git clone https://github.com/pirmansx/mbf
-sleep 1
 echo "\033[31;1m SUCCES..."
 cd mbf
 python2 MBF.py
 fi
 
-
-if [ $LB = 9 ] || [ $LB = 9 ]
+if [ $LB = 99 ] || [ $LB = 99 ]
 then
 cd $HOME
 pkg install cmatrix -y
 pkg install sl -y
 sl
 cmatrix -C cyan -a -b -s 
-cmatrix -C red -a -b -s
-cmatrix -C cyan -a -b -s -l 
-cmatrix -C red -a -b -s -l 
 cd LB && sh LB.sh
+fi
+
+if [ $LB = 00 ] || [ $LB = 00 ]
+then
+sleep 1
+echo "\033[32;1mThanks:D"
+sleep 01
+echo "\033[31;1mKELUAR"
+exit
 fi
